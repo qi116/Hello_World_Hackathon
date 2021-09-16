dc585689718768f52c739121423a6710050b8111
qi116 <qi116@purdue.edu>
2021-09-16 17:13:26 -0400

    Added Income input bar

--- Functions.js
+++ Functions.js
@@ -1,4 +1,9 @@
+var income;
 function makeWord(){
 	var word = document.getElementById("word");
 	word.style.display = "block";
+}
+function getIncome(){
+	var incomeInput = document.getElementById("userIncome").value;
+	console.log(incomeInput);
 }
